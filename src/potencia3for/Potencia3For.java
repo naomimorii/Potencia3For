950012bcbecf4d9616317e915483943e348e52d7
naomimorii <naomi.silva3@etec.sp.gov.br>
2020-06-22 19:08:23 -0300

    Inclusão Do Projeto

---
+++ src/potencia3for/Potencia3For.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package potencia3for;
+
+/**
+ *
+ * @author naomi
+ */
+public class Potencia3For {
+
+
+    public static void main(String[] args) {
+        double x;
+        int i;
+        
+        for(i=0;i<16;i++)
+        {
+            x= Math.pow(3,i);
+            System.out.print(x+"\n");
+            
+        }
+            
+    }
+    
+}
